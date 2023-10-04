d70b9a1e2a5ee2ad431ef65c544124557f535a4b
Devansh Arora <dev.marvel.avengers@gmail.com>
2023-10-05 02:20:47 +0530

    commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Image from 'next/image'
+import { NavbarDefault } from './components/Navbar'
+
+export default function Home() {
+	return (
+		<>
+			<NavbarDefault />
+			<div>
+				Portfolio
+			</div>
+		</>
+	);
+}
